36417453d7d583be1968d71a76fcda1ef869f833
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-07-11 14:15:12 -0300

    atualizando arquivos

--- TesteBackend/Program.cs
+++ TesteBackend/Program.cs
@@ -1,7 +1,16 @@
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+using Microsoft.EntityFrameworkCore;
+using MyAspNetCoreWebApp.Data; // Ajuste para o namespace correto do seu AppDbContext
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona o serviço do banco de dados MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 36)) // Ajuste a versão conforme seu MySQL
+    )
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
